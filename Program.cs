ecdfa3afe3065151bce093a3bb2cb8d8c99f4643
frizzle101101 <aaronfritz96@gmail.com>
2024-11-04 15:22:10 -0500

    Commit Solution

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿namespace _7_Reverse_Integer {
+    internal class Program {
+        static void Main(string[] args) {
+            Solution solution = new Solution();
+        }
+    }
+}
---
+++ Solution.cs
@@ -0,0 +1,13 @@
+﻿public class Solution {
+    public int Reverse(int x) {
+        var resultList = x.ToString().ToCharArray().ToList();
+        resultList.Reverse();
+        if (resultList[resultList.Count - 1] == '-') {
+            resultList.RemoveAt(resultList.Count - 1);
+            resultList.Insert(0, '-');
+        }
+        if (int.TryParse(new string(resultList.ToArray()), out int result))
+            return result;
+        return 0;
+    }
+}
